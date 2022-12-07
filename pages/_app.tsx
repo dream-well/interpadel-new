d25649b1977ab073694374890ebcc6155f9bde0c
dream-well <dreamwell506@outlook.com>
2022-12-08 01:34:36 +0800

    add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css'
+import 'rsuite/dist/rsuite.min.css';
+import 'styles/globals.css'
 import type { AppProps } from 'next/app'
+import Layout from 'components/Layout';
+import { Provider } from 'react-redux';
+import { store } from 'store/store';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
+  )
 }
