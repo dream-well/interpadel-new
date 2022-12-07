b160fbeb2a7ba1e701ee4e71123b3b0db0f8b472
dream-well <dreamwell506@outlook.com>
2022-12-08 01:49:59 +0800

    define colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,14 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      "black": "#0A0A0A",
+      "dark": "#1D1829",
+      "green": "#C2FF00",
+      "grey-dark": "#1F2024",
+      "grey": "#4A4654",
+      "grey-light": "#2C303A"
+    },
     extend: {},
   },
   plugins: [],
