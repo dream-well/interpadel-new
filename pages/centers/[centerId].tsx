5ebe96ba49c1da7dc58f00a0d1451fa0ef06e7bb
dream-well <dreamwell506@outlook.com>
2022-12-08 23:07:44 +0800

    fix

--- pages/centers/[centerId].tsx
+++ pages/centers/[centerId].tsx
@@ -1,26 +1,18 @@
-import PaymentCard from "components/PaymentCard";
-import { useEffect, useState } from "react";
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js";
 import { axios, fetcher } from "utils/helpers";
-import { DatePicker, SelectPicker } from "rsuite";
+import { Button, DatePicker, Modal, Placeholder, SelectPicker } from "rsuite";
 import useSWR from "swr";
 import { useRouter } from "next/router";
 import moment from "moment";
+import React, { useState } from "react";
 
 export default function Center() {
   
   const router = useRouter();
   const { centerId } = router.query;
-  const { data: center } = useSWR(`/api/centers/${centerId}`, fetcher);
+  const { data: center } = useSWR(centerId ? `/api/centers/${centerId}` : null, fetcher);
   const closeAt = moment.duration(center?.closeAt).hours();
   const openAt = moment.duration(center?.openAt).hours();
   const hours = closeAt - openAt;
-
-  const book = (court, i) => {
-    const startAt = openAt + i;
-    
-  }
 
   return (
     <div className='py-[5rem] px-[10rem] flex flex-col justify-center'>
@@ -29,39 +21,14 @@ export default function Center() {
       </div>
       <div className='w-full bg-dark rounded-b-[1.5rem] pb-[3.5rem]'>
         <div className='h-[5rem] p-6 justify-between w-full flex text-white items-center'>
-          <DatePicker value={new Date} cleanable={false} />
+          <DatePicker cleanable={false} />
           <div className='flex items-center'>
             <img src='/images/icons/location.svg' className='h-5' />
             <span className='ml-2'>Helsingborg</span>
           </div>
         </div>
         <div className='px-6 text-light'>
-          <table className='w-full saira'>
-            <tr>
-              <td className='border pl-4 py-2 w-[7.5rem]'>Time/Courts</td>
-              {
-                center?.courts.map((court, key) => (
-                  <td key={key} className='border py-2 text-center'>
-                    { court.name }
-                  </td>
-                ))
-              }
-            </tr>
-            {
-              (new Array(hours)).fill(0).map((_, i) => (
-                <tr key={i}>
-                  <td className='border pl-4'>{moment(`2000-01-01 ${openAt + i}:00`).format('h:mm a')}</td>
-                  {
-                    center?.courts.map((court, key) => (
-                      <td key={key} className='border py-2 text-center h-12' onClick={() => book(court, i)}>
-                        {/* { court.name } */}
-                      </td>
-                    ))
-                  }   
-                </tr>
-              ))
-            }
-          </table>
+          <SlotTable openAt={openAt} hours={hours} courts={center?.courts} />
           <div className='flex justify-end items-center text-sm mt-6 space-x-4'>
             <div className='flex items-center'>
               <div className='bg-dark border rounded-[0.125rem] w-[0.8rem] h-[0.8rem] mr-1'></div>
@@ -79,5 +46,63 @@ export default function Center() {
         </div>
       </div>
     </div>
+  )
+}
+
+function SlotTable({ openAt, hours = 0, courts = [] }) {
+  const [open, setOpen] = useState(false);
+  return (
+    <div>
+      <table className='w-full saira'>
+        <tbody>
+          <tr>
+            <td className='border pl-4 py-2 w-[7.5rem]'>Time/Courts</td>
+            {
+              courts.map((court, key) => (
+                <td key={key} className='border py-2 text-center'>
+                  { court.name }
+                </td>
+              ))
+            }
+          </tr>
+          {
+            (new Array(hours)).fill(0).map((_, i) => (
+              <tr key={i}>
+                <td className='border pl-4'>{moment(`2000-01-01 ${openAt + i}:00`).format('h:mm a')}</td>
+                {
+                  courts.map((court, key) => (
+                    <td key={key} className='border py-2 text-center h-12' onClick={() => setOpen(true)}>
+                      {/* { court.name } */}
+                    </td>
+                  ))
+                }   
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+      <DurationSelector open={open} setOpen={setOpen} />
+    </div>
+  )
+}
+
+function DurationSelector({ open, setOpen}) {
+  return (
+    <Modal open={open} onClose={() => setOpen(false)}>
+      <Modal.Header>
+        <Modal.Title>Modal Title</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Placeholder.Paragraph />
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={() => setOpen(false)} appearance="primary">
+          Ok
+        </Button>
+        <Button onClick={() => setOpen(false)} appearance="subtle">
+          Cancel
+        </Button>
+      </Modal.Footer>
+    </Modal>
   )
 }
