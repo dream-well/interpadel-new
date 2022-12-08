6dd6bdce8925d480f0072aaeba838b38eba1a759
PassionFriend <divas1635@gmail.com>
2022-12-07 23:35:02 -0500

    initial update

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -4,56 +4,60 @@ import SearchIcon from '@rsuite/icons/Search';
 export default function Home() {
   return (
     <>
-      <div className="w-full"><img src="/images/home/main.png"/></div>
-      <div className="font-Saira text-[4rem] -mt-[23rem] text-white flex justify-center items-center flex-col">
-        <div>Find where & with whom</div><div>to play Padel & Tennis instantly</div>
-      </div>
       <Search />
-      <div className='mt-[8.75rem]'>
-        <div className="flex justify-center items-center font-Saira text-[3rem] flex-col">
-          <div>Top Searched Clubs in Norway</div><div>Book Now!</div>
-        </div>
-        <div className="flex justify-center items-center font-sans text-[1rem] flex-col mt-[1.5rem] text-[#4A4654]">
-          <div>Interpadel Shop sells most padel equipment for both beginners and the more experienced player. The</div>
-          <div>online store has finally opened, and you will find many good offers on various equipment.</div>
-        </div>
-      </div>
-
+      <SectionText />
       <Section type="left" url="/images/home/book1.png"/>
       <Section type="right" url="/images/home/book2.png"/>
       <Section type="left" url="/images/home/book3.png"/>
       <EasierForYou />
+      <Trustedbythousands />
+      <Testimonial />
+      <Download />
+      <FAQ />
+      <GetStarted />
     </>
   )
 }
 
 function Search() {
+  const data = ["padel1", "padel2"].map(item => ({label: item, value: item}));
   return (
-    <div className="mx-[8.5rem] rounded-3xl shadow-2xl h-[11rem] mt-[6rem] bg-white flex p-[2.5rem]">
-        <div className="border border-[#D2D1D4] rounded-2xl py-[1rem] mr-[2.5rem]">
-          <span className='mx-[1.5rem] font-Saira font-semibold text-[1.25rem]'>Address</span>
-          <Input className= "mx-[1.5rem] font-normal text-[1rem] w-fit" placeholder='Address, club name, city...'/>
-        </div>
-        <div className="border border-[#D2D1D4] rounded-2xl py-[1rem] w-[17.5rem] mr-[2.5rem]">
-          <span className='mx-[1.5rem] font-Saira font-semibold text-[1.25rem]'>Select Sport</span><br/>
-          <InputPicker className= "mx-[1.5rem] font-normal text-[1rem]" data={[{lable: "Padel", value: "Padel"}]}/>
-        </div>
-        <div className="border border-[#D2D1D4] rounded-2xl py-[1rem] w-[17.5rem]">
-          <span className='mx-[1.5rem] font-Saira font-semibold text-[1.25rem]'>Date & time</span><br/>
-          <DatePicker className= "mx-[1.5rem] font-normal text-[1rem] w-fit" />
-        </div>
-        <div className="flex justify-center items-center py-[1rem] w-[17.5rem]">
-          <Button appearance='primary' className='!text-black !bg-green' size="lg">
-            Search&nbsp;&nbsp;&nbsp;<SearchIcon/>
-          </Button>
-        </div>
+    <div className="flex justify-between rounded-3xl z-10 shadow-2xl h-[11rem] -mt-[5.5rem] bg-white p-[2.5rem]">
+      <div className='rounded-2xl border px-[1.5rem] py-[1rem]'>
+        <div className="font-semibold text-[1.25rem] mb-[0.5rem]">Address</div>
+        <Input placeholder='Address, club name, city...' className='text-[1rem]'/>
       </div>
+      <div className='rounded-2xl border px-[1.5rem] py-[1rem]'>
+        <div className="font-semibold text-[1.25rem] mb-[0.5rem]">Select Sport</div>
+        <InputPicker data={data} />
+      </div>
+      <div className='rounded-2xl border px-[1.5rem] py-[1rem]'>
+        <div className="font-semibold text-[1.25rem] mb-[0.5rem]">Date & Time</div>
+        <DatePicker />
+      </div>
+      <div className='justify-center items-center flex'>
+        <Button appearance='primary' className='!bg-green !text-black'>Search <SearchIcon/></Button>
+      </div>
+    </div>
+  );
+}
+function SectionText() {
+  return (
+  <div className='mt-[8.75rem]'>
+    <div className="flex justify-center items-center font-Saira text-[3rem] flex-col">
+      <div>Top Searched Clubs in Norway</div><div>Book Now!</div>
+    </div>
+    <div className="flex justify-center items-center font-sans text-[1rem] flex-col mt-[1.5rem] text-[#4A4654]">
+      <div>Interpadel Shop sells most padel equipment for both beginners and the more experienced player. The</div>
+      <div>online store has finally opened, and you will find many good offers on various equipment.</div>
+    </div>
+  </div>
   );
 }
 function Section(props) {
   return(
     props.type == "left" ? 
-  <div className='mx-[8.5rem] text-white flex my-[2.5rem]'>
+  <div className='text-white flex my-[2.5rem]'>
       <img src={props.url}></img>
       <div className='bg-dark rounded-3xl pt-[2.625rem] px-[6.25rem] pb-[3rem] h-[18.75rem] -ml-[10.625rem] mt-[6.25rem]'>
         <div className='flex justify-between'>
@@ -71,7 +75,7 @@ function Section(props) {
       </div>
     </div>
     :
-    <div className='mx-[8.5rem] text-white flex my-[2.5rem]'>
+    <div className='text-white flex my-[2.5rem]'>
       <div className='bg-dark rounded-3xl z-10 pt-[2.625rem] px-[6.25rem] pb-[3rem] h-[18.75rem] mt-[6.25rem]'>
         <div className='flex justify-between'>
           <div className='font-Saira text-[2rem] font-semibol'>Sanset Padel</div>
@@ -91,8 +95,22 @@ function Section(props) {
   );
 }
 function EasierForYou() {
+  const data = [
+    {
+      title: "Find Here",
+      text: "Interpadel Shop sells most padel equipm for both beginners and the more experienced player. The online store has finally opened, and you will find many good offers on various equipment."
+    },
+    {
+      title: "Booking",
+      text: "Interpadel Shop sells most padel equipm for both beginners and the more experienced player. The online store has finally opened, and you will find many good offers on various equipment."
+    },
+    {
+      title: "Community Inspire Us",
+      text: "Interpadel Shop sells most padel equipm for both beginners and the more experienced player. The online store has finally opened, and you will find many good offers on various equipment."
+    }
+  ]
   return (
-    <div className='bg-dark py-[5rem] px-[8.5rem]'>
+    <div className='bg-dark py-[5rem]'>
       <div className='flex justify-center items-center'>
         <span className='font-Saira font-semibold text-[3rem] text-white'>
           Interpadel Makes It 
@@ -101,18 +119,135 @@ function EasierForYou() {
           &nbsp;Easier For You
         </span>
       </div>
+      <div className='flex space-x-[1.875rem] justify-center'>
+        {
+          data.map((item, key) => (
+            <div className='mt-[3.75rem] w-[23.125rem] flex flex-col justify-center items-center' key={key}>
+              <div className='z-10 rounded-full bg-grey-light border-dark border-[0.5rem] w-[6rem] h-[6rem] text-green flex items-center justify-center text-[3rem] font-semibold'>
+                {"0"}{key + 1}
+              </div>
+              <div className=' bg-grey-light h-[19rem] rounded-3xl -mt-[3rem] flex flex-col justify-center items-center px-[1.75rem]'>
+                <div className='text-white text-[1.75rem] font-semibold mb-[1rem]'>{item.title}</div>
+                <div className='text-white text-[1rem] font-normal text-center'>{item.text}</div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    </div>
+  );
+}
+
+function Trustedbythousands() {
+  return (
+    <div className='bg-green text-black py-[5rem]'>
+      <div className='flex justify-between'>
+        <div className='text-[3rem] font-semibold'>
+          Trusted by thousands
+        </div>
+        <div className='flex items-center space-x-[1.5rem]'>
+          <div className='border-black border rounded-[0.3rem] px-[1rem] py-[0.5rem]'>
+            <img src="images/home/companies/01.png"/>
+          </div>
+          <div className='border-black border rounded-[0.3rem] px-[1rem] py-[0.5rem]'>
+            <img src="images/home/companies/02.png"/>
+          </div>
+          <div className='border-black border rounded-[0.3rem] px-[1rem] py-[0.5rem]'>
+            <img src="images/home/companies/03.png"/>
+          </div>
+        </div>
+      </div>
+      <div className='flex space-x-[4.625rem] mt-[1.5rem]'>
+        <div className='w-[24rem] text-[1rem] font-normal font-sans opacity-70'>
+        Over 1 million users. Used by thousands of teams, companies and organizations worldwide.
+        </div>
+        <div className='flex items-center space-x-[1.5rem]'>
+          <div className='border-black border rounded-[0.3rem] px-[1rem] py-[0.5rem]'>
+            <img src="images/home/companies/04.png"/>
+          </div>
+          <div className='border-black border rounded-[0.3rem] px-[1rem] py-[0.5rem]'>
+            <img src="images/home/companies/05.png"/>
+          </div>
+          <div className='border-black border rounded-[0.3rem] px-[1rem] py-[0.5rem]'>
+            <img src="images/home/companies/06.png"/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Testimonial() {
+  return (
+    <div className='py-[8.75rem] flex'>
+      <div className='z-10 mt-[7.5rem]'>
+        <img src="images/home/quote.png"/>
+      </div>
+      <div className='py-[7.5rem] pl-[13.375rem] pr-[2.75rem] text-white bg-dark -ml-[10.625rem] flex flex-col w-[45rem]'>
+        <div>Testimonial</div>
+        <div className='text-[3rem] font-semibold'>Our Customer Says</div>
+        <div>
+          <img src="images/home/qutesMark.png"/>
+        </div>
+        <div className='mt-[1.375rem]'>
+          Great job on my Cakephp scraper script! Easy to communicate with and even did a few additional tasks that were not apart of the project that were greatly appreciated! Will be hiring again for sure. ;-) 
+        </div>
+        <div className='flex mt-[1.876rem]'>
+          <img src="images/home/quoteWoman.png"/>
+          <div className='flex flex-col ml-[1.5rem]'>
+            <div className='text-[1.25rem] font-semibold'>Elenor Rose</div>
+            <div>Manager</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Download() {
+  return(
+    <div className='flex bg-dark text-white py-[1.5rem]'>
+      <div className='flex justify-center flex-col'>
+        <div className='font-semibold text-[3rem]'>Download our mobile app</div>
+        <div className='w-[28.563rem] mt-[1.5rem]'>Disposable temporary email protects your real email address from spam, advertising mailings, malwares.
+</div>
+        <div className='mt-[3.75rem] flex space-x-[2.215rem]'>
+          <img src="images/home/googleBadge.png"/>
+          <img src="images/home/appBadge.png"/>
+        </div>
+      </div>
+      <div>
+        <img src="images/home/iphone.png"/>
+      </div>
+    </div>
+  );
+}
+
+function FAQ() {
+  return (
+    <div className="flex flex-col text-white items-center justify-center">
+      <div className='text-[3rem] font-semibold mt-[8.75rem]'>
+        Frequently Asked Questions
+      </div>
+      <div className='w-[48.125rem] font-normal text-center mt-[1.5rem]'>
+        Over a dozen reusable features built to provide iconography, dropdowns, input groups, alerts, and much more.Over a dozen reusable features
+      </div>
       <div className='flex'>
-        <div className=' mt-[3.75rem]'>
-          <div className='rounded-full bg-grey-light border-dark border-[0.5rem] w-[6rem] h-[6rem]'></div>
-          <div className='w-[21.875rem] bg-grey-light h-[19rem] rounded-3xl'></div>
-        </div>
-        <div className=' mt-[3.75rem]'>
-          <div className='rounded-full bg-grey-light border-dark border-[0.5rem] w-[6rem] h-[6rem]'></div>
-          <div className='w-[21.875rem] bg-grey-light h-[19rem] rounded-3xl'></div>
-        </div>
-        <div className=' mt-[3.75rem]'>
-          <div className='rounded-full bg-grey-light border-dark border-[0.5rem] w-[6rem] h-[6rem]'></div>
-          <div className='w-[21.875rem] bg-grey-light h-[19rem] rounded-3xl'></div>
+        <img src="images/home/faq.png"/>
+      </div>
+    </div>
+  );
+}
+
+function GetStarted() {
+  return (
+    <div className='bg-white text-black flex mt-[16rem] px-[1.75rem] py-[3rem]'>
+      <img src="images/home/getstarted.png" className='-mt-[7.3rem]'/>
+      <div className='flex flex-col ml-[3rem]'>
+        <div className='font-semibold text-[3rem]'>Your data security is our first priority, explore now!</div>
+        <div className='mt-[1.5rem] opacity-70 w-[28.1rem]'>Enjoy the most powerful cloud storage. Provide security for all your data</div>
+        <div>
+          <Button appearance='primary' className='!bg-green !text-black mt-[2.688rem] w-[12.125rem] h-[3.75rem]'>Get Started Now</Button>
         </div>
       </div>
     </div>
