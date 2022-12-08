4bb9d651485556f55041cb7383539866571a4521
crazydevlegend <crazy010323@gmail.com>
2022-12-08 05:56:20 -0500

    upcomings, team section

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,6 +1,11 @@
 import Link from 'next/link'
-import { Button } from 'rsuite'
+import { Button, Badge, Progress } from 'rsuite'
 import EmailIcon from '@rsuite/icons/Email';
+import CalendarIcon from '@rsuite/icons/Calendar';
+import LocationIcon from '@rsuite/icons/Location';
+import CreditCardPlusIcon from '@rsuite/icons/CreditCardPlus';
+import CreditCardMinusIcon from '@rsuite/icons/CreditCardMinus';
+import MinusIcon from '@rsuite/icons/Minus';
 
 export default function Profile() {
   return (
@@ -10,9 +15,9 @@ export default function Profile() {
             avatar='images/profile/avatar.png'
             rate={5.0}
         />
-        <div className='flex justify-between space-x-[3rem]'>
+        <div className='flex justify-between space-x-[3.813rem]'>
             {/* left space */}
-            <div className='flex flex-col space-y-[3rem] w-1/4'>
+            <div className='flex flex-col space-y-[2.5rem] w-[18.75rem]'>
                 <Collection name='Favorite Venues'>
                     {favoriteVenues.map((favoriteVenue, key) =>(
                         <FavoriteCard {...favoriteVenue} key={key}/>
@@ -30,8 +35,10 @@ export default function Profile() {
                 </Collection>
             </div>
             {/* right space */}
-            <div className='flex flex-col space-y-[3rem] w-3/4'>
-                right space
+            <div className='flex flex-col space-y-[2.5rem] flex-grow'>
+                <UpcomingBooking />
+                <UpcomingActivity />
+                <TeamMembers />
             </div>
         </div>
     </div>
@@ -42,13 +49,18 @@ const Summary = ({name, avatar, rate}) => {
     return (
         <div className='flex p-[3rem] space-x-[3rem] bg-[#1d1829] justify-between'>
             <div className='flex flex-col w-1/3 space-y-[3rem]'>
-                <div className='flex justify-between'>
-                    <img src={avatar} alt='User Avatar' />
-                    <span className='font-bold text-[2rem] text-white'>{name}</span>
+                <div className='flex justify-between items-center'>
+                    <Badge content={rate.toFixed(1)} color={'green'}>
+                        <img src={avatar} alt='User Avatar' />
+                    </Badge>
+                    <span className='font-bold text-[2.625rem] text-white'>{name}</span>
                 </div>
-                <div className='flex flex-col rounded-3xl border-grey border-2 p-[2rem] space-y-[1rem]'>
+                <div className='flex flex-col rounded-3xl border-grey border-2 p-[2.5rem] space-y-[1rem]'>
                     <span className='font-bold text-white text-[1.5rem]'>Your next booking</span>
-                    <span className='text-white'>You have no upcoming bookings</span>
+                    <span className='text-white flex items-center space-x-2'>
+                        <CalendarIcon className='text-[1.5rem]' />
+                        <span>You have no upcoming bookings</span>
+                    </span>
                 </div>
                 <Button appearance="ghost" className='w-[15rem] h-[3rem] rounded-xl bg-[#c2ff00] !border-green text-black'>
                     <Link href='/profile/edit'>+ Book New Time Slot</Link>
@@ -68,21 +80,29 @@ const FavoriteCard = ({image, name, location}) => (
         <img src={image} alt='Venue' className='w-[3rem] h-[3rem] rounded-md'></img>
         <div className='flex flex-col'>
             <span>{name}</span>
-            <span>{location}</span>
+            <span className='flex space-x-2 items-center'>
+                <LocationIcon/>
+                <span>{location}</span>
+            </span>
         </div>
     </div>
 )
 
 const MatchingCard = ({avatar, name, location, rate, matching}) => (
     <div className='flex bg-[#1d1829] space-x-5 p-[1rem] text-white'>
-        <img src={avatar} alt='Venue' className='w-[3rem] h-[3rem] rounded-full'></img>
-        <div className='flex flex-col'>
-            <div className='flex justify-between'>
-                
-                <span>{name}</span>
+        <img src={avatar} alt='Venue' className='w-[2.5rem] h-[2.5rem] rounded-full'></img>
+        <div className='flex flex-col flex-grow space-y-2'>
+            <div className='flex space-x-5 justify-between'>
+                <span className='rounded-md bg-green text-black px-[0.5rem] py-[0.2rem] text-[0.75rem]'>{rate.toFixed(1)}</span>
+                <span className='text-[#F4F3F4] items-center'>{name}</span>
                 <EmailIcon className='justify-end' />
             </div>
-            <span>{location}</span>
+            <span className='flex space-x-2 items-center'>
+                <LocationIcon/>
+                <span>{location}</span>
+            </span>
+            <Progress.Line percent={matching} showInfo={false} className='px-0' />
+            <span className='0.875rem'>{name.split(' ')[0]} Matches your profile {matching}%</span>
         </div>
     </div>
 )
@@ -94,19 +114,90 @@ const Collection = ({name, children}) => (
     </div>
 )
 
+const UpcomingBooking = () => {
+    return (
+        <div className='flex flex-col p-[2.5rem] bg-dark space-y-[3.313rem]'>
+            <span className='text-white flex items-center space-x-2 text-[1.75rem] font-bold'>
+                <CalendarIcon />
+                <span>Upcoming Booking</span>
+            </span>
+            <span className='flex text-white'>
+                You have no upcoming bookings. Search for available times above.
+            </span>
+        </div>
+    )
+}
+
+const UpcomingActivity = () => {
+    return (
+        <div className='flex flex-col p-[2.5rem] bg-dark space-y-[3.313rem]'>
+            <span className='text-white flex items-center space-x-2 text-[1.75rem] font-bold'>
+                <CalendarIcon />
+                <span>Upcoming Activities</span>
+            </span>
+            <span className='flex text-white'>
+                You have no upcoming activities.
+            </span>
+        </div>
+    )
+}
+
+const TeamMember = ({avatar, name, location, rate, team}) => (
+    <div className='flex bg-[#2c303a] space-x-[1rem] p-[2rem] text-white items-center rounded-xl'>
+        <img src={avatar} alt='Venue' className='w-[2.5rem] h-[2.5rem] rounded-full'></img>
+        <div className='flex flex-col space-y-2'>
+            <div className='flex space-x-5 justify-between'>
+                <span className='rounded-md bg-green text-black px-[0.5rem] py-[0.2rem] text-[0.75rem]'>{rate.toFixed(1)}</span>
+                <span className='text-[#F4F3F4] items-center'>{name}</span>
+            </div>
+            <span className='flex space-x-2 items-center'>
+                <LocationIcon/>
+                <span>{location}</span>
+            </span>
+        </div>
+        <span className='flex flex-grow justify-center'>{team}</span>
+        <Button appearance='ghost' className='flex rounded-xl border border-green text-green px-[1.5rem] py-[0.75rem] items-center space-x-2'>
+            <span>Remove</span>
+            <MinusIcon />
+        </Button>
+    </div>
+)
+
+const TeamMembers = () => {
+    return (
+        <div className='flex flex-col p-[2.5rem] bg-dark space-y-[2.5rem]'>
+            <div className='flex justify-between'>
+                <span className='text-white flex items-center space-x-2 text-[1.75rem] font-bold'>
+                    <CalendarIcon />
+                    <span>Your Team Members</span>
+                </span>
+                <Button className='flex rounded-xl bg-green text-black px-[1.5rem] py-[0.75rem] items-center space-x-2'>
+                    <Link href='/profile/edit'>Make a new team </Link>
+                    <CreditCardPlusIcon />
+                </Button>
+            </div>
+            <div className='flex flex-col space-y-[1rem]'>
+                {members.map((member, index) =>(
+                    <TeamMember {...member} key={index} />
+                ))}
+            </div>
+        </div>
+    )
+}
+
 const favoriteVenues = [
     {
-        image: '/images/profile/favoriteVenus/1.png',
+        image: '/images/profile/favorites/1.png',
         name: '247 PADEL Vala centrum',
         location: 'Helsingburg',
     },
     {
-        image: '/images/profile/favoriteVenus/2.png',
+        image: '/images/profile/favorites/2.png',
         name: '247 PADEL Vala centrum',
         location: 'Helsingburg',
     },
     {
-        image: '/images/profile/favoriteVenus/3.png',
+        image: '/images/profile/favorites/3.png',
         name: '247 PADEL Vala centrum',
         location: 'Helsingburg',
     },
@@ -114,24 +205,55 @@ const favoriteVenues = [
 
 const matchingPlayers = [
     {
-        avatar: '/images/profile/matchingPlayers/1.png',
+        avatar: '/images/profile/matchings/1.png',
         rate: 5,
         name: 'Tobias Ribba',
         location: 'Helsingburg',
         matching: 49,
     },
     {
-        avatar: '/images/profile/matchingPlayers/2.png',
+        avatar: '/images/profile/matchings/2.png',
         rate: 4,
         name: 'Tobias Ribba',
         location: 'Helsingburg',
         matching: 56,
     },
     {
-        avatar: '/images/profile/matchingPlayers/3.png',
+        avatar: '/images/profile/matchings/3.png',
         rate: 6,
         name: 'Tobias Ribba',
         location: 'Helsingburg',
         matching: 70,
+    },
+]
+
+const members = [
+    {
+        avatar: '/images/profile/team/1.png',
+        rate: 5,
+        name: 'Tobias Ribba',
+        location: 'Helsingburg',
+        team: 'Team name 1',
+    },
+    {
+        avatar: '/images/profile/team/2.png',
+        rate: 4,
+        name: 'Tobias Ribba',
+        location: 'Helsingburg',
+        team: 'Team name 1',
+    },
+    {
+        avatar: '/images/profile/team/3.png',
+        rate: 6,
+        name: 'Tobias Ribba',
+        location: 'Helsingburg',
+        team: 'Team name 1',
+    },
+    {
+        avatar: '/images/profile/team/4.png',
+        rate: 3,
+        name: 'Tobias Ribba',
+        location: 'Helsingburg',
+        team: 'Team name 1',
     },
 ]
