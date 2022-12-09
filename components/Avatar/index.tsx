f0f9ac3b6742b1d7162fbb4d0e19583dd62185d1
crazydevlegend <crazy010323@gmail.com>
2022-12-09 02:45:57 -0500

    add Avatar

--- components/Avatar/index.tsx
+++ components/Avatar/index.tsx
@@ -2,6 +2,6 @@ import cn from 'classnames';
 
 export default function Avatar({ src, alt='Avatar', className }) {
     return (
-        <img src={src} alt={alt} className={cn('rounded-full object-cover', className)}/>
+        <img src={src} alt={alt} className={cn('rounded-full object-cover object-center border border-solid border-grey', className)}/>
     )
 }
