664eef65522c3583bed9c73b44feddff04e73dc0
crazydevlegend <crazy010323@gmail.com>
2022-12-10 21:56:09 -0500

    fixed booking page

--- pages/books/index.tsx
+++ pages/books/index.tsx
@@ -1,11 +1,8 @@
-import axios from 'axios';
-import { now } from 'moment';
 import Link from 'next/link'
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Badge, Calendar, Whisper, Popover } from 'rsuite'
 import useSWR from 'swr';
 import { fetcher } from 'utils/helpers';
-// import CalendarIcon from '@rsuite/icons/Calendar';
 
 export default function Books() {
 
@@ -20,72 +17,73 @@ export default function Books() {
 const BookCalendar = () => {
 
   const [currentDate, setCurrentDate] = useState(new Date());
-  const { data: bookings, error } = useSWR(`/api/bookings?month=${currentDate.getFullYear()}-${currentDate.getMonth()}`, fetcher)
+  const { data: bookings } = useSWR(`/api/profile/bookings?month=${currentDate.getFullYear()}-${currentDate.getMonth() + 1}`, fetcher)
 
   const handleSelect = (newDate) => {
     setCurrentDate(newDate);
   }
 
-  useEffect(() => {
-    console.log(bookings);
-  }, [currentDate])
-  
-
   function getTodoList(date) {
-      const day = date.getDate();
+    const data = bookings?.filter(booking => {
+      const startTime = new Date(booking.startAt);
+      return (
+        (startTime.getFullYear() === date.getFullYear()) && 
+        (startTime.getMonth() === date.getMonth()) &&
+        (startTime.getDate() === date.getDate())
+      );
+    })
+    console.log(date, data);
     
-      switch (day) {
-        case 10:
-          return [
-            { time: '10:30 am', title: 'Meeting' },
-            { time: '10:30 am', title: 'Meeting' },
-          ];
-        case 15:
-          return [
-            { time: '09:30 pm', title: 'Products Introduction' },
-          ];
-        default:
-          return [];
-      }
+    return data;
   }
 
   function renderCell(date) {
-      const list = getTodoList(date);
-      const displayList = list.filter((item, index) => index < 2);
-  
-      if (list.length) {
-          const moreCount = list.length - displayList.length;
-          const moreItem = (
-          <li>
-              <Whisper
-                  placement="top"
-                  trigger="click"
-                  speaker={
-                      <Popover>
-                      {list.map((item, index) => (
-                          <p key={index}>
-                          <b>{item.time}</b> - {item.title}
-                          </p>
-                      ))}
-                      </Popover>
-                  }
-                  >
-                  <a>{moreCount} more</a>
-              </Whisper>
-          </li>
-          );
+    const list = getTodoList(date) || [];
+    const displayList = list.filter((item, index) => index < 2);
 
-          return (
-              <ul className="calendar-todo-list">
-                  {displayList.map((item, index) => (
-                  <li key={index}>
-                      <Badge /> <b>{item.time}</b> - {item.title}
-                  </li>
+    const BookingItem = ({data}) => {
+      const startTime = new Date(data.startAt);
+      return (
+        <Link
+          className='text-white'
+          href={`/centers/${data.center?._id}?date=${startTime.getFullYear()}-${startTime.getMonth()}-${startTime.getDay()}`}>
+          {startTime.getHours() + ':' + startTime.getMinutes()} - {data.center?.name}
+          <br/>
+        </Link>
+      )
+    }
+
+    if (list.length) {
+      const moreCount = list.length - displayList.length;
+      const moreItem = (
+      <li>
+          <Whisper
+              placement="top"
+              trigger="click"
+              speaker={
+                  <Popover>
+                  {list.map((item, index) => (
+                    <BookingItem data={item} key={index} />
                   ))}
-                  {moreCount ? moreItem : null}
-              </ul>
-          );
-      }
+                  </Popover>
+              }
+              >
+              <a>{moreCount} more</a>
+          </Whisper>
+      </li>
+      );
+
+      return (
+        <ul className="calendar-todo-list">
+          {displayList.map((item, index) => (
+          <li key={index}>
+            <Badge /> <BookingItem data={item} />
+          </li>
+          ))}
+          {moreCount ? moreItem : null}
+        </ul>
+      );
+    }
   }
 
   return (
