2e82f8f9586a1b6656596797f377bd882b685020
dream-well <dreamwell506@outlook.com>
2022-12-17 01:56:50 +0800

    pages

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 
-console.log("env", process.env);
-
 export default NextAuth({
   providers: [
     GoogleProvider({
--- pages/auth/login/index.tsx
+++ pages/auth/login/index.tsx
@@ -13,7 +13,6 @@ import Image from 'components/Image';
 import { getProviders, signIn } from "next-auth/react"
 
 export default function Login({ providers }) {
-    console.log(providers);
     const [formValue, setFormValue] = useState({
         email: '',
         password: ''
@@ -107,7 +106,7 @@ export default function Login({ providers }) {
                 
                 {
                 providers &&
-                Object.values(providers).map((provider) => (
+                Object.values(providers).map((provider: any) => (
                     <div key={provider.name}>
                     <button onClick={() => signIn(provider.id)}>
                         Sign in with {provider.name}
