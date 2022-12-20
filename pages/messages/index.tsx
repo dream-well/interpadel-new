8721a11ea068a28e9d1a79f768d5da653eb159af
crazydevlegend <crazy010323@gmail.com>
2022-12-20 00:20:20 -0500

    show message chat / send message

--- pages/messages/index.tsx
+++ pages/messages/index.tsx
@@ -1,33 +1,33 @@
 import SearchIcon from '@rsuite/icons/Search';
-import cn from 'classnames';
 import Avatar from 'components/Avatar';
 import useApi from 'hooks/useApi';
-import { useState } from 'react';
-import { Input, InputGroup, List } from 'rsuite'
+import {  useState } from 'react';
+import { Button, Input, InputGroup, List, useToaster } from 'rsuite'
+import SendIcon from '@rsuite/icons/Send';
+import axios from 'axios';
+import { notification } from 'utils/helpers';
+import cn from 'classnames';
 
 export default function Messages() {
-    const { data: users } = useApi('/api/users')
     
     const [currentUser, setCurrentUser] = useState({})
     const [query, setQuery] = useState('')
 
-    const handleSearch = () => {
-        alert(query)
-    }
+    const { data: users } = useApi('/api/chats/contacts', { query: query || '' })
 
     return (
-        <div className='flex px-[8.5rem]'>
+        <div className='flex px-[8.5rem] bg-grey-dark'>
             <div className='flex flex-col w-1/4 border-r-2 p-[1rem]'>
                 <SearchBox
                     query={query}
-                    onChange={setQuery}
-                    onSearch={handleSearch}
+                    onSearch={setQuery}
                 />
                 <Users
                     users={users}
                     currentUser={currentUser}
                     onUserChange={setCurrentUser}
                 />
+                {users?.length === 0 && <span className='text-white'>No one in the contacts</span>}
             </div>
             <div className='w-full'>
                 <MessagePanel
@@ -38,23 +38,31 @@ export default function Messages() {
     )
 }
 
-const SearchBox = ({ query, onChange, onSearch}) => {
+const SearchBox = ({ query, onSearch }) => {
 
-    const CustomInputGroupWidthButton = ({ value, onChange, onPressEnter, placeholder, ...props }) => (
-        <InputGroup {...props} inside className='mb-[1rem]'>
-          <Input placeholder={placeholder} onPressEnter={onPressEnter} onChange={onChange} value={value} />
-          <InputGroup.Button onClick={onPressEnter}>
-            <SearchIcon />
-          </InputGroup.Button>
-        </InputGroup>
-    );
+    const CustomInputGroupWidthButton = ({ value, onPressEnter, placeholder, ...props }) => {
+        const [query, setQuery] = useState(value)
+
+        return (
+            <InputGroup {...props} inside className='mb-[1rem]'>
+            <Input
+                placeholder={placeholder} 
+                onPressEnter={() => onPressEnter(query)} 
+                onChange={setQuery} 
+                value={query}
+            />
+            <InputGroup.Button onClick={() => onPressEnter(query)}>
+                <SearchIcon />
+            </InputGroup.Button>
+            </InputGroup>
+        )
+    };
 
     return (
         <CustomInputGroupWidthButton 
             placeholder="Search players..."
-            onPressEnter={onSearch}
             value={query}
-            onChange={onChange}
+            onPressEnter={onSearch}
         />
     )
 }
@@ -74,6 +82,7 @@ const Users = ({users, currentUser, onUserChange, className=''}) => {
             </List.Item>
         )
     }
+
     return (
         <List className='rounded-[0.5rem] text-white'>
         {users?.map(user => (
@@ -90,14 +99,39 @@ const Users = ({users, currentUser, onUserChange, className=''}) => {
 
 const MessagePanel = ({ currentUser }) => {
 
+    const { data: messages, mutate } = useApi(`/api/chats/${currentUser._id}`)
+    const [message, setMessage] = useState('')
+
+    const toaster = useToaster();
+
+    const sendMessage = (evt) => {
+        evt.preventDefault();
+        if ( message.length === 0 ) return;
+
+        axios.post(`/api/chats/${currentUser._id}`, { message })
+        .then(data => {
+            mutate();
+            setMessage('')
+        })
+        .catch(err => {
+            toaster.push(
+                notification({
+                    title: "Messages",
+                    description: "Could not send messages due to some issues",
+                    type: "error",
+                }),
+                { placement: 'topEnd', }
+            )
+        })
+    }
+
     const InfoPanel = ({user}) => {
         
         const name = user.firstname + ' ' + user.lastname;
 
         const Info = ({detail, value}) => (
             <span>
-                <span className='font-bold'>{detail}</span> :
-                <span>{value}</span>
+                <span className='font-bold'>{detail}</span> : <span>{value}</span>
             </span>
         )
         
@@ -108,16 +142,47 @@ const MessagePanel = ({ currentUser }) => {
                     <span>{name}</span>
                 </div>
                 <div className='flex space-x-[2rem]'>
-                    <Info detail='Level' value={user.level} />
                     <Info detail='Email' value={user.email} />
+                    <Info detail='Address' value={user.address} />
                 </div>
             </div>
         )
     }
 
+    const ChatElement = ( { chat, className='' } ) => {
+        if (chat?.message === '')   return <></>
+        return (
+            <div className={cn('m-1 p-2 bg-grey rounded-[0.5rem] w-auto', chat?.to === currentUser?._id ? "text-right" : "text-left")}>
+                {chat.message}
+            </div>
+        )
+    }
+
     return (
-        <div className='flex flex-col'>
+        <div className='flex flex-col text-white'>
             {currentUser?._id && <InfoPanel user={currentUser} />}
+            {messages?.length > 0 && (
+                <div className='p-2'>
+                    {messages.map((msg, index) => <ChatElement key={index} chat={msg} />)}
+                </div>
+            )}
+            {messages?.length === 0 && (
+                <span>No Messages with this player</span>
+            )}
+            <div className='flex space-x-2 p-3 border-t-2'>
+                <Input 
+                    as="textarea"
+                    rows={3}
+                    placeholder="Message..."
+                    value={message}
+                    onChange={setMessage}
+                    className='w-grow align-bottom'
+                    onClick={sendMessage}
+                />
+                <Button color="green" appearance="primary" onClick={sendMessage}>
+                    <SendIcon /> Send
+                </Button>
+            </div>
         </div>
     )
 }
